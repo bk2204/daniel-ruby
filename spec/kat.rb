c02661f4ff18779703d51b7b1fda77ebeec1fd47
brian m. carlson <sandals@crustytoothpaste.net>
2014-02-01 18:33:49 +0000

    Test Unicode handling.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/kat.rb
+++ spec/kat.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/ruby
+# encoding: UTF-8
 
 $:.unshift File.join(File.dirname(__FILE__), '..')
 
@@ -8,6 +9,9 @@ describe Daniel::PasswordGenerator do
   [
     ["foo", "bar", "3*Re7n*qcDDl9N6y", "8244c50a1000bar"],
     ["foo", "baz", "Dp4iWIX26UwV55N(", "8244c50a1000baz"],
+    # Test Unicode.
+    ["La République française", "la-france", "w^O)Vl7V0O&eEa^H",
+     "55b1d40a1000la-france"],
   ].each do |items|
     master, code, result, reminder = items
     it "gives the expected password for #{master}, #{code}" do
