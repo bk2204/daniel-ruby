d7feec9f3b3eaef47aeeee08eba11c4c351def5d
brian m. carlson <sandals@crustytoothpaste.net>
2014-02-04 01:54:29 +0000

    Add a function to convert flags parameters.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/character_set.rb
+++ spec/character_set.rb
@@ -27,4 +27,51 @@ describe Daniel::CharacterSet do
       set.should_not include x
     end
   end
+  context "in from_characters" do
+    it "returns the same thing as new for a Fixnum" do
+      (0..Daniel::CharacterSet::SYMBOL_MASK).each do |value|
+        set = Daniel::CharacterSet.new value
+        cs = Daniel::CharacterSet.from_characters value
+        (0x0..0xff).each do |x|
+          set.include?(x).should eq cs.include?(x)
+        end
+      end
+    end
+    it "converts a string to an integer if it is decimal" do
+      (0..Daniel::CharacterSet::SYMBOL_MASK).each do |value|
+        set = Daniel::CharacterSet.new value
+        cs = Daniel::CharacterSet.from_characters value.to_s
+        (0x0..0xff).each do |x|
+          set.include?(x).should eq cs.include?(x)
+        end
+      end
+    end
+    it "converts a string to an integer if it is octal" do
+      (0..Daniel::CharacterSet::SYMBOL_MASK).each do |value|
+        set = Daniel::CharacterSet.new value
+        cs = Daniel::CharacterSet.from_characters "0" + value.to_s(8)
+        (0x0..0xff).each do |x|
+          set.include?(x).should eq cs.include?(x)
+        end
+      end
+    end
+    it "converts a string to an integer if it is hexadecimal" do
+      (0..Daniel::CharacterSet::SYMBOL_MASK).each do |value|
+        set = Daniel::CharacterSet.new value
+        cs = Daniel::CharacterSet.from_characters "0x" + value.to_s(16)
+        (0x0..0xff).each do |x|
+          set.include?(x).should eq cs.include?(x)
+        end
+      end
+    end
+    it "contains only letters and numbers for 'a0'" do
+      csc = Daniel::CharacterSet
+      mask = csc::NO_SPACES | csc::NO_SYMBOLS_TOP | csc::NO_SYMBOLS_OTHER
+      set = Daniel::CharacterSet.new mask
+      cs = Daniel::CharacterSet.from_characters "a0"
+      (0x0..0xff).each do |x|
+        set.include?(x).should eq cs.include?(x)
+      end
+    end
+  end
 end
