453b0892545f6aca0aca1e982b2cd4c723858782
brian m. carlson <sandals@crustytoothpaste.net>
2014-09-04 22:38:08 +0000

    Add tests to parse reminders for existing passwords.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/large-params.rb
+++ spec/large-params.rb
@@ -12,9 +12,11 @@ describe Daniel::PasswordGenerator do
     ['bar', 'ca67960a1000la-france', 'ca6796', 10, 16, 0, 'la-france'],
     ['foo', '8244c50c3264la-france', '8244c5', 12, 50, 100, 'la-france'],
     ['foo', '8244c50c810001la-france', '8244c5', 12, 128, 1, 'la-france'],
-    ['foo', '8244c50c8100ab22la-france', '8244c5', 12, 128, 5538, 'la-france']
+    ['foo', '8244c50c8100ab22la-france', '8244c5', 12, 128, 5538, 'la-france'],
+    ['foo', '8244c5200301000000la-france', '8244c5', 32, 3, 1, 'la-france',
+     "\x00\x00\x00"]
   ].each do |items|
-    password, reminder, csum, flags, length, version, code = items
+    password, reminder, csum, flags, length, version, code, mask = items
     it "gives the expected values for #{reminder}" do
       pieces = Daniel::Reminder.parse(reminder)
       expect(pieces[:checksum]).to eq(csum)
@@ -27,7 +29,7 @@ describe Daniel::PasswordGenerator do
     it "generates the expected values of #{reminder}" do
       gen = Daniel::PasswordGenerator.new(password)
       params = Daniel::Parameters.new(flags, length, version)
-      expect(gen.reminder(code, params)).to eq(reminder)
+      expect(gen.reminder(code, params, mask)).to eq(reminder)
     end
   end
 end
