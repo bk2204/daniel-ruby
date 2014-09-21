50ab94d5cda739fa59561e96dfc0fb340fc80a8d
brian m. carlson <sandals@crustytoothpaste.net>
2014-09-21 18:53:51 +0000

    Remove trailing comma.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/formatter.rb
+++ spec/formatter.rb
@@ -9,7 +9,7 @@ describe Daniel::Formatter do
   [
     ['', 'xexax'],
     ['1234567890', 'xesef-disof-gytuf-katof-movif-baxux'],
-    ['Pineapple', 'xigak-nyryk-humil-bosek-sonax'],
+    ['Pineapple', 'xigak-nyryk-humil-bosek-sonax']
   ].each do |(plain, bubblebabble)|
     it "performs no transformation for plain '#{plain}'" do
       res = Daniel::Formatter.plain(plain)
