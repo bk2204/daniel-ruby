33910d69753356dd4bc5621f75eff21545873d87
brian m. carlson <sandals@crustytoothpaste.net>
2015-04-10 22:46:25 +0000

    Fix coverage reporting.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,16 @@
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 
-require 'daniel'
-
 # This has to two separate if statements or Opal won't ignore it.
 if RUBY_ENGINE != 'opal'
+  # Coverage has to go before other requires.
   if ENV['COVERAGE']
     require 'simplecov'
 
     SimpleCov.start 'rails'
   end
 end
+
+require 'daniel'
 
 RSpec.configure do |c|
   c.full_backtrace = true
