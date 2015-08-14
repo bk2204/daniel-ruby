a13a399eb2182a21ff60b04ce67e4455bec807e9
brian m. carlson <sandals@crustytoothpaste.net>
2015-08-14 22:35:11 +0000

    spec: reduce duplication in existing spec
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/existing_spec.rb
+++ spec/existing_spec.rb
@@ -15,29 +15,30 @@ describe Daniel::PasswordGenerator do
     master, code, mask, result, remroot = items
     rawmask = Daniel::Util.from_hex(mask)
     reminder = [remroot, mask, code].join
+    testname = "#{master}, #{mask}, #{code}"
 
-    it "gives the expected password for #{master}, #{mask}, #{code}" do
+    it "gives the expected password for #{testname}" do
       gen = Daniel::PasswordGenerator.new master
       params = Daniel::Parameters.new(Daniel::Flags::REPLICATE_EXISTING,
                                       result.length)
       expect(gen.generate(code, params, rawmask)).to eq(result)
     end
 
-    it "gives the expected mask for password for #{master}, #{mask}, #{code}" do
+    it "gives the expected mask for password for #{testname}" do
       gen = Daniel::PasswordGenerator.new master
       params = Daniel::Parameters.new(Daniel::Flags::REPLICATE_EXISTING,
                                       result.length)
       expect(gen.generate_mask(code, params, result)).to eq(rawmask)
     end
 
-    it "gives the expected reminder for #{master}, #{mask}, #{code}" do
+    it "gives the expected reminder for #{testname}" do
       gen = Daniel::PasswordGenerator.new master
       params = Daniel::Parameters.new(Daniel::Flags::REPLICATE_EXISTING,
                                       result.length)
       expect(gen.reminder(code, params, rawmask)).to eq(reminder)
     end
 
-    it "gives the expected password for #{master}, #{mask}, #{code} reminder" do
+    it "gives the expected password for #{testname} reminder" do
       gen = Daniel::PasswordGenerator.new master
       expect(gen.generate_from_reminder(reminder)).to eq(result)
     end
