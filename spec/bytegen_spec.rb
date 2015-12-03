0c749b25c17dc67985274b9d689e79a7f9a6ea1c
brian m. carlson <sandals@crustytoothpaste.net>
2015-12-03 01:50:01 +0000

    bytegen: add more test vectors.
    Add additional test vectors for the HMAC_DRBG implementation.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/bytegen_spec.rb
+++ spec/bytegen_spec.rb
@@ -28,6 +28,22 @@ if RUBY_ENGINE != 'opal'
           'b924159413e2293b17610f211e09f770f172b8fb693a35b85d3b9e5e63b1dc25' \
           '2ac0e115002e9bedfb4b5b6fd43f33b8e0eafb2d072e1a6fee1f159df9b51e6c' \
           '8da737e60d5032dd30544ec51558c6f080bdbdab1de8a939e961e06b5f1aca37'
+      ],
+      [
+        'b340907445b97a8b589264de4a17c0bea11bb53ad72f9f33297f05d2879d898d',
+        '65cb27735d83c0708f72684ea58f7ee5',
+        '75183aaaf3574bc68003352ad655d0e9ce9dd17552723b47fab0e84ef903694a' \
+          '32987eeddbdc48efd24195dbdac8a46ba2d972f5808f23a869e71343140361f5' \
+          '8b243e62722088fe10a98e43372d252b144e00c89c215a76a121734bdc485486' \
+          'f65c0b16b8963524a3a70e6f38f169c12f6cbdd169dd48fe4421a235847a23ff'
+      ],
+      [
+        '8e159f60060a7d6a7e6fe7c9f769c30b98acb1240b25e7ee33f1da834c0858e7',
+        'c39d35052201bdcce4e127a04f04d644',
+        '62910a77213967ea93d6457e255af51fc79d49629af2fccd81840cdfbb491099' \
+          '1f50a477cbd29edd8a47c4fec9d141f50dfde7c4d8fcab473eff3cc2ee9e7cc9' \
+          '0871f180777a97841597b0dd7e779eff9784b9cc33689fd7d48c0dcd341515ac' \
+          '8fecf5c55a6327aea8d58f97220b7462373e84e3b7417a57e80ce946d6120db5'
       ]
     ].each_with_index do |(hsalt, hsecret, houtput), i|
       it "passes test vector #{i}" do
