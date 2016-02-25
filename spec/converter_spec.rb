0a2eb70a63c595c946d50816d7d3c0519f44e93e
brian m. carlson <sandals@crustytoothpaste.net>
2016-02-25 00:34:00 +0000

    Exclude converter from being run by Opal.
    The module has been explicitly excluded because it isn't useful in a
    pure JavaScript environment.  Don't try to run the tests either.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -1,10 +1,12 @@
 # Ruby 1.8 doesn't have require_relative.
 require File.join(File.dirname(__FILE__), 'spec_helper')
 
-describe Daniel::Converter do
-  it 'properly converts from one master password to another' do
-    c = Daniel::Converter.new('foo', 'foobar')
-    reminder = c.convert('8244c50a1000bar')
-    expect(reminder).to eq '72eb362010005120687ae64c642fdfc988448ced0d19bar'
+if ::RUBY_ENGINE != 'opal'
+  describe Daniel::Converter do
+    it 'properly converts from one master password to another' do
+      c = Daniel::Converter.new('foo', 'foobar')
+      reminder = c.convert('8244c50a1000bar')
+      expect(reminder).to eq '72eb362010005120687ae64c642fdfc988448ced0d19bar'
+    end
   end
 end
