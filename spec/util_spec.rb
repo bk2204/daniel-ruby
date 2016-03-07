fb6503dbd410ca929677721e652a625b33ac8edf
brian m. carlson <sandals@crustytoothpaste.net>
2016-03-07 02:53:09 +0000

    Add additional base64 and url64 tests.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/util_spec.rb
+++ spec/util_spec.rb
@@ -21,23 +21,31 @@ describe Daniel::Util do
     expect(Daniel::Util.to_base64('abcdefg')).to eq 'YWJjZGVmZw=='
     expect(Daniel::Util.to_base64('hij')).to eq 'aGlq'
     expect(Daniel::Util.to_base64("klmn\n")).to eq 'a2xtbgo='
+    expect(Daniel::Util.to_base64('aa?')).to eq 'YWE/'
+    expect(Daniel::Util.to_base64('bc>')).to eq 'YmM+'
   end
 
   it 'converts data from base64 as expected' do
     expect(Daniel::Util.from_base64('YWJjZGVmZw==')).to eq 'abcdefg'
     expect(Daniel::Util.from_base64('aGlq')).to eq 'hij'
     expect(Daniel::Util.from_base64('a2xtbgo=')).to eq "klmn\n"
+    expect(Daniel::Util.from_base64('YWE/')).to eq 'aa?'
+    expect(Daniel::Util.from_base64('YmM+')).to eq 'bc>'
   end
 
   it 'converts data to url64 as expected' do
     expect(Daniel::Util.to_url64('abcdefg')).to eq 'YWJjZGVmZw'
     expect(Daniel::Util.to_url64('hij')).to eq 'aGlq'
     expect(Daniel::Util.to_url64("klmn\n")).to eq 'a2xtbgo'
+    expect(Daniel::Util.to_url64('aa?')).to eq 'YWE_'
+    expect(Daniel::Util.to_url64('bc>')).to eq 'YmM-'
   end
 
   it 'converts data from url64 as expected' do
     expect(Daniel::Util.from_url64('YWJjZGVmZw')).to eq 'abcdefg'
     expect(Daniel::Util.from_url64('aGlq')).to eq 'hij'
     expect(Daniel::Util.from_url64('a2xtbgo')).to eq "klmn\n"
+    expect(Daniel::Util.from_url64('YWE_')).to eq 'aa?'
+    expect(Daniel::Util.from_url64('YmM-')).to eq 'bc>'
   end
 end
