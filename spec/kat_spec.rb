2e6a4b7b27902bee2d60a02c280e74175ba3a77c
brian m. carlson <sandals@crustytoothpaste.net>
2016-03-20 13:39:54 +0000

    opal: mark Unicode master password tests as pending.
    Opal doesn't deal well with UTF-8 strings that are converted to binary.
    As a result, the byte values don't end up matching and Unicode master
    password strings are broken.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/kat_spec.rb
+++ spec/kat_spec.rb
@@ -5,6 +5,10 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe Daniel::PasswordGenerator do
+  def known_failure(code)
+    ::RUBY_ENGINE == 'opal' && code == 'la-france'
+  end
+
   [
     ['foo', 'bar', '3*Re7n*qcDDl9N6y', '8244c50a1000bar'],
     ['foo', 'baz', 'Dp4iWIX26UwV55N(', '8244c50a1000baz'],
@@ -14,14 +18,22 @@ describe Daniel::PasswordGenerator do
   ].each do |items|
     master, code, result, reminder = items
     it "gives the expected password for #{master}, #{code}" do
+      pending 'Opal encoding issues' if known_failure(code)
+
       gen = Daniel::PasswordGenerator.new master
       expect(gen.generate(code, Daniel::Parameters.new(10))).to eq(result)
     end
+
     it "gives the expected reminder for #{master}, #{code}" do
+      pending 'Opal encoding issues' if known_failure(code)
+
       gen = Daniel::PasswordGenerator.new master
       expect(gen.reminder(code, Daniel::Parameters.new(10))).to eq(reminder)
     end
+
     it "gives the expected password for #{master}, #{code} reminder" do
+      pending 'Opal encoding issues' if known_failure(code)
+
       gen = Daniel::PasswordGenerator.new master
       expect(gen.generate_from_reminder(reminder)).to eq(result)
     end
