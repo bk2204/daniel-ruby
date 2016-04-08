4f27a781d22364184f273f3bd03fa9b05b52f5b4
brian m. carlson <sandals@crustytoothpaste.net>
2016-04-08 01:09:59 +0000

    Add additional comprehensive tests.
    These take a set of parameters from a colon-separated value file and
    ensure that whatever way you mix and match them, they end up correct.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

---
+++ spec/comprehensive_spec.rb
@@ -0,0 +1,82 @@
+#!/usr/bin/ruby
+# encoding: UTF-8
+
+# Ruby 1.8 doesn't have require_relative.
+require File.join(File.dirname(__FILE__), 'spec_helper')
+
+def entries
+  # TODO: implement IO#each_line as a polyfill
+  return [] if ::RUBY_ENGINE == 'opal'
+  f = File.new(File.join(File.dirname(__FILE__), 'fixtures', 'reminders.csv'),
+               'r')
+  f.each_line.map do |line|
+    next if /^\s*#/.match(line)
+    line.chomp.split(':').map { |s| unescape(s) }
+  end.reject(&:nil?)
+end
+
+def unescape(s)
+  # Basically, CGI.unescape.
+  s = s.gsub(/%([0-9a-fA-F]{2})/) do
+    Daniel::Util.from_hex(Regexp.last_match[1])
+  end
+  Daniel::Util.to_binary(s)
+end
+
+describe Daniel::PasswordGenerator do
+  def known_failure(pass)
+    ::RUBY_ENGINE == 'opal' && !pass.bytes.select { |b| b > 0x7f }.empty?
+  end
+
+  entries.each do |entry|
+    master, csum, flags, ver, code, reminder, result, description = *entry
+    csum = Daniel::Util.to_binary(csum)
+    result = Daniel::Util.to_binary(result)
+    flags = flags.unpack('w')[0]
+    ver = ver.unpack('w')[0]
+
+    it "gives the expected checksum for #{description}" do
+      pending 'Opal encoding issues' if known_failure(master)
+
+      gen = Daniel::PasswordGenerator.new master
+      expect(gen.checksum).to eq csum
+    end
+
+    it "gives the expected password for #{description}" do
+      pending 'Opal encoding issues' if known_failure(master)
+
+      gen = Daniel::PasswordGenerator.new master
+      rem = gen.parse_reminder(reminder)
+      expect(gen.generate_from_reminder(reminder)).to eq(result)
+      expect(gen.generate_from_reminder(rem)).to eq(result)
+    end
+
+    unless /^000000/.match(reminder)
+      it "round-trips the reminder correctly for #{description}" do
+        pending 'Opal encoding issues' if known_failure(master)
+
+        gen = Daniel::PasswordGenerator.new master
+        rem = gen.parse_reminder(reminder)
+        expect(gen.reminder(rem.code, rem.params, rem.mask)).to eq(reminder)
+      end
+    end
+
+    it "parses the reminder correctly for #{description}" do
+      pending 'Opal encoding issues' if known_failure(master)
+
+      gen = Daniel::PasswordGenerator.new master
+      rem = gen.parse_reminder(reminder)
+      expect(rem.code).to eq code
+      expect(rem.params.flags).to eq flags
+      expect(rem.params.version).to eq ver
+    end
+
+    it "round-trips passwords correctly for #{description}" do
+      pending 'Opal encoding issues' if known_failure(master)
+
+      gen = Daniel::PasswordGenerator.new master
+      rem = gen.parse_reminder(reminder)
+      expect(gen.generate(rem.code, rem.params, rem.mask)).to eq(result)
+    end
+  end
+end
