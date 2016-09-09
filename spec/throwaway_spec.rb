8aba8cebca6bffba5267e686c69c9708c01ba940
brian m. carlson <sandals@crustytoothpaste.net>
2016-09-09 16:09:40 +0000

    spec: improve throwaway spec
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/throwaway_spec.rb
+++ spec/throwaway_spec.rb
@@ -23,8 +23,22 @@ if RUBY_ENGINE != 'opal'
     it 'should generate valid passwords' do
       io = StringIO.new('', 'w')
       prog = MockMainProgram.new(io)
-      prog.main(%w(bar baz))
+      prog.main(%w(-p bar baz))
       expect(io.string).to eq "3*Re7n*qcDDl9N6y\nDp4iWIX26UwV55N("
+    end
+
+    it 'should generate machine-readable passwords with -r' do
+      io = StringIO.new('', 'w')
+      prog = MockMainProgram.new(io)
+      prog.main(%w(-pr bar baz))
+      expect(io.string).to eq "3%2ARe7n%2AqcDDl9N6y\nDp4iWIX26UwV55N%28"
+    end
+
+    it 'should generate different passwords with -v' do
+      io = StringIO.new('', 'w')
+      prog = MockMainProgram.new(io)
+      prog.main(%w(-p -v1 bar baz))
+      expect(io.string).to eq "YME0d&bl2N&#tk*g\n&@r6A8BdRQgJeY@u"
     end
   end
 end
